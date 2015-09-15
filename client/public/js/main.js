7c61b380fbc45f80d93eb4d817a30fd3ba134c29
omaracrystal <omaracrystal@gmail.com>
2015-09-14 20:33:45 -0600

    add llama pics and some styling

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -2,8 +2,8 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
-getLlamas();
-
+  getLlamas();
+  llamaPics();
 });
 
 //Submit button, create a llama
@@ -33,12 +33,19 @@ function getLlamas(){
  $.get('/api/llamas', function(data){
    for (var i = 0; i < data.length; i++) {
       $('#all-llamas').append(
-        '<tr><td>' + data[i].name + '</td><td>' + data[i].age + '</td><td>' + data[i].spitter + '</td></tr>' +    '<td><a class="btn btn-danger btn-xs delete-button" id="' + data[i]._id + '" role="button">Delete</a>' +
-       '&nbsp;<a class="btn btn-primary btn-xs edit-button" id="' + data[i]._id + '" role="button">Edit</a></td>' +
-       '</tr>'
+        '<tr><td>' + data[i].name + '</td><td>' + data[i].age + '</td><td>' + data[i].spitter + '</td><td>' + '<a class="btn btn-danger btn-xs delete-button" id="' + data[i]._id + '" role="button">Delete</a>' + '</td><td><a class="btn btn-primary btn-xs edit-button" id="' + data[i]._id + '" role="button">Edit</a></td></tr>'
      );
    }
       $('form input').val('');
       $('#spitter').removeAttr('checked');
  });
+}
+
+
+function llamaPics(){
+  var myPhoto = ["llama1.jpeg", "llama2.jpeg", "llama3.jpeg", "llama4.jpeg", "llama5.jpeg", "llamaMeme.jpeg"];
+  $('#llama-pics').html('');
+  for (var i = 0; i < myPhoto.length; i++) {
+    $('#llama-pics').append('<img src="js/images/' + myPhoto[i] + '" style="width:200px;height:250px;">');
+  };
 }
--- server/models/llamas.js
+++ server/models/llamas.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
 var Llama = new Schema({
     name: String,
     age: Number,
     spitter: Boolean
 });
 
-//pull info from .env file
+
+//connect to mongoose and pull info from .env file
 mongoose.connect(process.env.MONGO_URI); //|| 'mongodb://localhost/...'
 
 module.exports = mongoose.model("llamas", Llama)
